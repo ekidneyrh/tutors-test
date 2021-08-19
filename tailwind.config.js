a7224c7dd7cdc45148bdba210060814c41167ba7
EmmaKidney <ekidney@redhat.com>
2021-08-19 12:48:56 +0100

    added refining colours pdf

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    // add DaisyUI plugin
+    plugins: [
+      require('daisyui'),
+    ],
+
+    // config (optional)
+    daisyui: {
+      styled: true,
+      themes: true,
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+    },
+
+  }
+
+  
